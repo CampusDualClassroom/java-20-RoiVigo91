90b6ce6ade8e74767e76d7bb4669c144c8cd46f0
RoiVigo91 <roioliveira71@gmail.com>
2024-12-19 19:58:18 +0100

    Ejercicio20Resuelto

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,18 +6,27 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        List<Person> stringList = new ArrayList<>();
+
+        stringList.add(new Person("John", "Smith"));
+        stringList.add(new Teacher("María", "Montessori", "Educación"));
+        stringList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        stringList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+
+        return stringList;
 
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person person : stringList) {
+            person.getDetails();
+        }
     }
 
     public static void main(String[] args) {
         List<Person> stringList = getPeopleList();
         showPeopleDetails(stringList);
     }
-
 
 
 }
